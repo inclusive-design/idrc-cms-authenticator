a4b25581d050296ff7e0e3781c32950cdfd8031e
Ned Zimmerman <ned@bight.dev>
2024-06-25 10:56:55 -0300

    feat: refactor based on sveltia-cms-auth

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 require("dotenv").config({ silent: true });
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const middleware = require("./middleware/index.js");
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(cookieParser());
+
+app.get("/", (_req, res) => {
+	res.status(404).send();
+});
 
 app.get("/auth", middleware.auth);
 app.get("/callback", middleware.callback);
-app.get("/success", middleware.success);
-app.get("/", middleware.index);
 
-app.listen(port, () => {
-  console.log(`IDRC CMS Authenticator listening on port ${port}.`);
-});
+app.listen(port);
