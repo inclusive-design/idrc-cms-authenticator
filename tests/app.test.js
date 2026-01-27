e772dd28d11bb0c3cf34322ed97d1bc8f492d544
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-01-27 16:03:13 -0400

    chore(deps): use @inclusive-design linters (#282)

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,232 +1,231 @@
-import process from "node:process";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import express from "express";
-import request from "supertest";
-import { assert, expect, onTestFinished, test } from "vitest";
-import middleware from "../middleware/index.js";
+import process from 'node:process';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import request from 'supertest';
+import {
+	assert, expect, onTestFinished, test,
+} from 'vitest';
+import middleware from '../middleware/index.js';
 
-const { randomUUID } = require("node:crypto");
+import {randomUUID} from 'node:crypto';
 
-dotenv.config({ path: ".env.test", silent: true });
+dotenv.config({path: '.env.test', silent: true});
 
-test("auth route fails without valid provider", async () => {
+test('auth route fails without valid provider', async () => {
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/auth", middleware.auth);
+	app.get('/auth', middleware.auth);
 
-	const res = await request(app)
-		.get("/auth?provider=forgejo&site_id=example.com")
+	const response = await request(app)
+		.get('/auth?provider=forgejo&site_id=example.com')
 		.send();
 
-	assert.isFalse(res.redirect);
-	expect(res.text).toMatch(/UNSUPPORTED_BACKEND/);
+	assert.isFalse(response.redirect);
+	expect(response.text).toMatch(/UNSUPPORTED_BACKEND/);
 });
 
-test("auth route fails without valid domain", async () => {
+test('auth route fails without valid domain', async () => {
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/auth", middleware.auth);
+	app.get('/auth', middleware.auth);
 
-	const res = await request(app)
-		.get("/auth?provider=github&site_id=example.test")
+	const response = await request(app)
+		.get('/auth?provider=github&site_id=example.test')
 		.send();
-	assert.isFalse(res.redirect);
-	expect(res.text).toMatch(/UNSUPPORTED_DOMAIN/);
+	assert.isFalse(response.redirect);
+	expect(response.text).toMatch(/UNSUPPORTED_DOMAIN/);
 });
 
-test("auth route fails without client ID", async () => {
+test('auth route fails without client ID', async () => {
 	const clientId = process.env.OAUTH_CLIENT_ID;
-	process.env.OAUTH_CLIENT_ID = "";
+	process.env.OAUTH_CLIENT_ID = '';
 
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/auth", middleware.auth);
+	app.get('/auth', middleware.auth);
 
-	const res = await request(app)
-		.get("/auth?provider=github&site_id=example.com")
+	const response = await request(app)
+		.get('/auth?provider=github&site_id=example.com')
 		.send();
-	assert.isFalse(res.redirect);
-	expect(res.text).toMatch(/MISCONFIGURED_CLIENT/);
+	assert.isFalse(response.redirect);
+	expect(response.text).toMatch(/MISCONFIGURED_CLIENT/);
 	onTestFinished(() => {
 		process.env.OAUTH_CLIENT_ID = clientId;
 	});
 });
 
-test("auth route fails without client secret", async () => {
+test('auth route fails without client secret', async () => {
 	const clientSecret = process.env.OAUTH_CLIENT_SECRET;
-	process.env.OAUTH_CLIENT_SECRET = "";
+	process.env.OAUTH_CLIENT_SECRET = '';
 
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/auth", middleware.auth);
+	app.get('/auth', middleware.auth);
 
-	const res = await request(app)
-		.get("/auth?provider=github&site_id=example.com")
+	const response = await request(app)
+		.get('/auth?provider=github&site_id=example.com')
 		.send();
-	assert.isFalse(res.redirect);
-	expect(res.text).toMatch(/MISCONFIGURED_CLIENT/);
+	assert.isFalse(response.redirect);
+	expect(response.text).toMatch(/MISCONFIGURED_CLIENT/);
 	onTestFinished(() => {
 		process.env.OAUTH_CLIENT_SECRET = clientSecret;
 	});
 });
 
-test("GitHub auth route succeeds with valid provider and site id", async () => {
+test('GitHub auth route succeeds with valid provider and site id', async () => {
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/auth", middleware.auth);
+	app.get('/auth', middleware.auth);
 
-	const res = await request(app)
-		.get("/auth?provider=github&site_id=example.com")
+	const response = await request(app)
+		.get('/auth?provider=github&site_id=example.com')
 		.send();
 
-	assert.isTrue(res.redirect);
-	expect(res.header.location).toMatch(
-		/https:\/\/github.com\/login\/oauth\/authorize\?client_id=client-id&scope=repo%2Cuser&state=[a-zA-Z0-9_-]+/,
-	);
+	assert.isTrue(response.redirect);
+	expect(response.header.location).toMatch(/https:\/\/github.com\/login\/oauth\/authorize\?client_id=client-id&scope=repo%2Cuser&state=[a-zA-Z0-9_-]+/);
 });
 
-test("GitLab auth route succeeds with valid provider and site id", async () => {
-	process.env.GIT_HOSTNAME = "gitlab.com";
+test('GitLab auth route succeeds with valid provider and site id', async () => {
+	process.env.GIT_HOSTNAME = 'gitlab.com';
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/auth", middleware.auth);
+	app.get('/auth', middleware.auth);
 
-	const res = await request(app)
-		.get("/auth?provider=gitlab&site_id=example.com")
+	const response = await request(app)
+		.get('/auth?provider=gitlab&site_id=example.com')
 		.send();
 
-	assert.isTrue(res.redirect);
-	expect(res.header.location).toMatch(
-		/https:\/\/gitlab.com\/oauth\/authorize\?client_id=client-id&redirect_uri=http%3A%2F%2F127\.0\.0\.1%2Fcallback&response_type=code&scope=api&state=[a-zA-Z0-9_-]+/,
-	);
+	assert.isTrue(response.redirect);
+	expect(response.header.location)
+		.toMatch(/https:\/\/gitlab.com\/oauth\/authorize\?client_id=client-id&redirect_uri=http%3A%2F%2F127\.0\.0\.1%2Fcallback&response_type=code&scope=api&state=[a-zA-Z0-9_-]+/);
 	onTestFinished(() => {
-		process.env.GIT_HOSTNAME = "github.com";
+		process.env.GIT_HOSTNAME = 'github.com';
 	});
 });
 
-test("callback route fails without valid provider", async () => {
-	const state = randomUUID().replaceAll("-", "");
+test('callback route fails without valid provider', async () => {
+	const state = randomUUID().replaceAll('-', '');
 
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/callback", middleware.callback);
+	app.get('/callback', middleware.callback);
 
-	const res = await request(app)
+	const response = await request(app)
 		.get(`/callback?code=123456789&state=${state}`)
 		.send();
 
-	expect(res.text).toMatch(/UNSUPPORTED_BACKEND/);
+	expect(response.text).toMatch(/UNSUPPORTED_BACKEND/);
 });
 
-test("callback route fails without code", async () => {
-	const state = randomUUID().replaceAll("-", "");
+test('callback route fails without code', async () => {
+	const state = randomUUID().replaceAll('-', '');
 
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/callback", middleware.callback);
+	app.get('/callback', middleware.callback);
 
-	const res = await request(app)
+	const response = await request(app)
 		.get(`/callback?state=${state}`)
-		.set("Cookie", [`csrf-token=github_${state}`])
+		.set('Cookie', [`csrf-token=github_${state}`])
 		.send();
 
-	expect(res.text).toMatch(/AUTH_CODE_REQUEST_FAILED/);
+	expect(response.text).toMatch(/AUTH_CODE_REQUEST_FAILED/);
 });
 
-test("callback route fails without CSRF token", async () => {
-	const state = randomUUID().replaceAll("-", "");
+test('callback route fails without CSRF token', async () => {
+	const state = randomUUID().replaceAll('-', '');
 
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/callback", middleware.callback);
+	app.get('/callback', middleware.callback);
 
-	const res = await request(app)
-		.get("/callback?code=123456789")
-		.set("Cookie", [`csrf-token=github_${state}`])
+	const response = await request(app)
+		.get('/callback?code=123456789')
+		.set('Cookie', [`csrf-token=github_${state}`])
 		.send();
 
-	expect(res.text).toMatch(/AUTH_CODE_REQUEST_FAILED/);
+	expect(response.text).toMatch(/AUTH_CODE_REQUEST_FAILED/);
 });
 
-test("callback route fails with CSRF mismatch", async () => {
-	const state = randomUUID().replaceAll("-", "");
-	const mismatchedState = randomUUID().replaceAll("-", "");
+test('callback route fails with CSRF mismatch', async () => {
+	const state = randomUUID().replaceAll('-', '');
+	const mismatchedState = randomUUID().replaceAll('-', '');
 
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/callback", middleware.callback);
+	app.get('/callback', middleware.callback);
 
-	const res = await request(app)
+	const response = await request(app)
 		.get(`/callback?code=123456789&state=${mismatchedState}`)
-		.set("Cookie", [`csrf-token=github_${state}`])
+		.set('Cookie', [`csrf-token=github_${state}`])
 		.send();
 
-	expect(res.text).toMatch(/CSRF_DETECTED/);
+	expect(response.text).toMatch(/CSRF_DETECTED/);
 });
 
-test("callback route fails without client ID", async () => {
+test('callback route fails without client ID', async () => {
 	const clientId = process.env.OAUTH_CLIENT_ID;
-	process.env.OAUTH_CLIENT_ID = "";
+	process.env.OAUTH_CLIENT_ID = '';
 
-	const state = randomUUID().replaceAll("-", "");
+	const state = randomUUID().replaceAll('-', '');
 
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/callback", middleware.callback);
+	app.get('/callback', middleware.callback);
 
-	const res = await request(app)
+	const response = await request(app)
 		.get(`/callback?code=123456789&state=${state}`)
-		.set("Cookie", [`csrf-token=github_${state}`])
+		.set('Cookie', [`csrf-token=github_${state}`])
 		.send();
 
-	expect(res.text).toMatch(/MISCONFIGURED_CLIENT/);
+	expect(response.text).toMatch(/MISCONFIGURED_CLIENT/);
 
 	onTestFinished(() => {
 		process.env.OAUTH_CLIENT_ID = clientId;
 	});
 });
 
-test("callback route fails without client secret", async () => {
+test('callback route fails without client secret', async () => {
 	const clientSecret = process.env.OAUTH_CLIENT_SECRET;
-	process.env.OAUTH_CLIENT_SECRET = "";
+	process.env.OAUTH_CLIENT_SECRET = '';
 
-	const state = randomUUID().replaceAll("-", "");
+	const state = randomUUID().replaceAll('-', '');
 
 	const app = express();
 
 	app.use(cookieParser());
 
-	app.get("/callback", middleware.callback);
+	app.get('/callback', middleware.callback);
 
-	const res = await request(app)
+	const response = await request(app)
 		.get(`/callback?code=123456789&state=${state}`)
-		.set("Cookie", [`csrf-token=github_${state}`])
+		.set('Cookie', [`csrf-token=github_${state}`])
 		.send();
 
-	expect(res.text).toMatch(/MISCONFIGURED_CLIENT/);
+	expect(response.text).toMatch(/MISCONFIGURED_CLIENT/);
 
 	onTestFinished(() => {
 		process.env.OAUTH_CLIENT_SECRET = clientSecret;
--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vitest/config";
+import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
 	coverage: {
-		reporter: [["clover", { file: "coverage/clover.xml" }], ["text"]],
+		reporter: [['clover', {file: 'coverage/clover.xml'}], ['text']],
 	},
 });
