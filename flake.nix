659ea4e1aa890ab0a6e2f058cb78302f8c1ebf7e
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-02-05 10:51:14 -0400

    chore(deps): use unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,6 +10,7 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
       in
       {
         devShells.default = pkgs.mkShell {
